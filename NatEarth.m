9194a5875fbea9d89402f2d21631aa73d501081f
Paul van Kan <pjmvkan@xs4all.nl>
2019-04-03 17:03:59 +0200

    Changes in natearth.m

--- NatEarth.m
+++ NatEarth.m
@@ -6,7 +6,7 @@ clc
 coast = shaperead('ne_110m_coastline\ne_110m_coastline.shp');
 h_coast = mapshow(coast, 'Color', 'red');
 geolines = shaperead('ne_110m_geographic_lines\ne_110m_geographic_lines.shp');
-h_geolines = mapshow(geolines, 'Color', 'black');
+h_geolines = mapshow(geolines, 'Color', 'black', 'LineStyle', ':');
 land = shaperead('ne_110m_land\ne_110m_land.shp');
 h_land = mapshow(land);
 %oceans = shaperead('ne_110m_ocean\ne_110m_ocean.shp');
